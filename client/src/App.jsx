8df45e4678a196a9be6a8afefd2860441ced8f48
Stoyan Boin <stoyan.boin@gmail.com>
2025-11-30 17:00:51 +0200

    Correction Shop Name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
                 <nav className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
                     <div className="container">
-                        <a className="navbar-brand" href="index.html">Furni<span>.</span></a>
+                        <a className="navbar-brand" href="index.html">Stoyan's Furniture<span>.</span></a>
 
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
