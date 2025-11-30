dd26bdd3402cb062ab0d799ac60af5e564e07ce4
Stoyan Boin <stoyan.boin@gmail.com>
2025-11-30 19:05:18 +0200

    Add component Hero

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route } from "react-router"
 import Header from "./components/header/Header.jsx"
 import Footer from "./components/footer/Footer.jsx"
+import Hero from "./components/hero/Hero.jsx"
 
 function App() {
 
@@ -9,27 +10,7 @@ function App() {
             <body>
 
                 <Header />
-
-                {/* <!-- Start Hero Section --> */}
-                <div className="hero">
-                    <div className="container">
-                        <div className="row justify-content-between">
-                            <div className="col-lg-5">
-                                <div className="intro-excerpt">
-                                    <h1>Modern Interior <span clsas="d-block">Design Studio</span></h1>
-                                    <p className="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</p>
-                                    <p><a href="" className="btn btn-secondary me-2">Shop Now</a><a href="#" className="btn btn-white-outline">Explore</a></p>
-                                </div>
-                            </div>
-                            <div className="col-lg-7">
-                                <div className="hero-img-wrap">
-                                    <img src="images/couch.png" className="img-fluid" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* <!-- End Hero Section --> */}
+                <Hero />
 
                 {/* <!-- Start Product Section --> */}
                 <div className="product-section">
@@ -383,7 +364,7 @@ function App() {
                     </div>
                 </div>
                 {/* <!-- End Blog Section -->	 */}
-                
+
                 <Footer />
 
                 <script src="js/bootstrap.bundle.min.js"></script>
