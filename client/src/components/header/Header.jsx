70a0916d936ff4e4e6d368e446e9d2df4e4c42eb
Stoyan Boin <stoyan.boin@gmail.com>
2025-12-13 15:54:32 +0200

    Fix home link

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
             <nav className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
                 <div className="container">
-                    <a className="navbar-brand" href="/">Stoyan's Furniture<span>.</span></a>
+                    <Link className="navbar-brand" to="/">Stoyan's Furniture<span>.</span></Link>
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -17,6 +17,8 @@ export default function Header() {
 
                     <div className="collapse navbar-collapse" id="navbarsFurni">
                         <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+
+
                             <li className="nav-item">
                                 <Link className="nav-link" to="/">Home</Link>
                             </li>
